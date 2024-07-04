a8e5b6b74459ab02edd4617929e20a6481860b47
KmanKid <david.kuehnemann@web.de>
2024-07-04 22:31:17 +0200

    hide console while executing

--- .vscode_base/runner.ps1
+++ .vscode_base/runner.ps1
@@ -3,11 +3,11 @@ param (
     [string]$outputParam
 )
 
-$inputParam = $inputParam -replace '\\','/'
-$outputParam = $outputParam -replace '\\','/'
+$inputParam = $inputParam -replace '\\', '/'
+$outputParam = $outputParam -replace '\\', '/'
 Write-Output "Starting build of $inputParam"
 # Define the command as a string
-$command = "!installpath!/usr/bin/bash.exe -c 'g++ -fdiagnostics-color=always -g $(pkg-config --cflags gtk4) $inputParam -o $outputParam $(pkg-config --libs gtk4)'"
+$command = "!installpath!/usr/bin/bash.exe -c 'g++ -mwindows -fdiagnostics-color=always -g $(pkg-config --cflags gtk4) $inputParam -o $outputParam $(pkg-config --libs gtk4)'"
 # Invoke the command
 Invoke-Expression $command
 Write-Output "Finished building $outputParam"
